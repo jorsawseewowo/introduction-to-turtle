1fd20dcdc982621b7c1816955d8a2d23970c5d92
jorsawseewowo <joachimandres.seydel@gmail.com>
2026-01-30 18:35:44 +0700

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("Red")
+sc=turtle.Screen()
+sc.setup(900, 800)
+turtle.title("Turtle No Plastic")
+board=turtle.Turtle()
+for i in range(100):
+    board.forward(100)
+    board.left(90)
+    i=i+1
